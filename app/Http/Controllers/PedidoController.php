4003ccd90d831ed18ce126f98ea48719def577c4
Diego Junio <diegojunio@gmail.com>
2026-06-12 09:45:44 -0300

    Removes comments

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -17,15 +17,7 @@ class PedidoController extends Controller
             'status' => 'status_pedido1',
         ]);
 
-        // $pedido2 = Pedido::create([
-        //     'cliente' => $request->cliente,
-        //     'valor_total' => $request->valor_total,
-        //     'status' => 'status_pedido2',
-        // ]);
-
-        // Envia para fila
         ProcessarPedidoJob::dispatch($pedido->id);
-        // ProcessarPedidoJob::dispatch($pedido2->id)->delay(now()->addMinutes(5));
 
         return response()->json([
             'message' => 'Pedido criado e enviado para processamento.',
